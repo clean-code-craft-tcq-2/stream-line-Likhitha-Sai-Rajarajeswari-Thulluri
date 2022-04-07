a22a710482d56c4d58edd2e2ce543ec26e6fbfaa
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-04-07 14:02:05 +0530

    Update SensorsDataReader.h

--- SensorsDataReader.h
+++ SensorsDataReader.h
@@ -3,6 +3,12 @@
 struct SensorsData{
   int temperatureSensorData;
   int pressureSensorData;
+  
+  bool operator==(const A &other) const {
+     if((temperatureSensorData==other.temperatureSensorData) && (pressureSensorData==other.pressureSensorData))
+        return true;
+     return false;
+    }
 };
   
 std::vector<SensorsData> readSensorsData();
