6099b489390bb049c4ffc907452ea899eea4f6c5
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-04-07 12:30:11 +0530

    Create SensorsDataReader.cpp

---
+++ SensorsDataReader.cpp
@@ -0,0 +1,13 @@
+#include<SensorsDataReader.h>
+
+std::vector<SensorsData> readSensorsData() {
+  std::vector<SensorsData> sensorsDataHolder;
+  struct SensorsData sensorsData;
+  sensorsData.temperatureSensorData = 21;
+  sensorsData.pressureSensorData = 15;
+  sensorsDataHolder.push_back(sensorsData);
+  sensorsData.temperatureSensorData = 25;
+  sensorsData.pressureSensorData = 5;
+  sensorsDataHolder.push_back(sensorsData);
+  return sensorsDataHolder;
+}
