1a2817f1a73152fd33cfb7c49a7787b86b73648c
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-04-13 22:44:31 +0530

    Update testSensorsDataReader.cpp

--- testSensorsDataReader.cpp
+++ testSensorsDataReader.cpp
@@ -2,22 +2,10 @@
 #include "test/catch.hpp"
 #include "SensorsDataReader.h"
 
-/*TEST_CASE("simulate and read data from sensors") {
-  std::vector<SensorsData> sensorsDataHolder;
-  struct SensorsData sensorsData;
-  sensorsData.temperatureSensorData = 21;
-  sensorsData.pressureSensorData = 15;
-  sensorsDataHolder.push_back(sensorsData);
-  sensorsData.temperatureSensorData = 25;
-  sensorsData.pressureSensorData = 5;
-  sensorsDataHolder.push_back(sensorsData);
-  REQUIRE(readSensorsData() == sensorsDataHolder);
-}*/
-
 TEST_CASE("simulate and read data from sensors") {
-  std::vector<SensorsData> sensorsDataHolder = readSensorsData();
+  std::vector<SensorsData> sensorsDataHolder = readSensorsData("BMSData.txt");
   
-  REQUIRE(36.4 == 36.4);
+  REQUIRE(sensorsDataHolder.size() == 50);
   
   struct SensorsData sensorsData;
   sensorsData.temperatureSensorData = sensorsDataHolder[44].temperatureSensorData;
