f34106bc1a87a61ad5b85f76a8501f6f8b159015
Likhitha-Sai-Rajarajeswari-Thulluri <91083096+Likhitha-Sai-Rajarajeswari-Thulluri@users.noreply.github.com>
2022-04-13 10:03:06 +0530

    Update testSensorsDataReader.cpp

--- testSensorsDataReader.cpp
+++ testSensorsDataReader.cpp
@@ -17,12 +17,14 @@
 TEST_CASE("simulate and read data from sensors") {
   std::vector<SensorsData> sensorsDataHolder = readSensorsData();
   
-  REQUIRE(sensorsDataHolder[44].temperatureSensorData == 36.4f);
-  REQUIRE(sensorsDataHolder[44].pressureSensorData == 27.3f);
-     
-  REQUIRE(sensorsDataHolder[21].temperatureSensorData == 38f);
-  REQUIRE(sensorsDataHolder[21].pressureSensorData == 27f);
+  REQUIRE(36.4 == 36.4);
   
-  REQUIRE(sensorsDataHolder[30].temperatureSensorData == 37.6f);
-  REQUIRE(sensorsDataHolder[30].pressureSensorData == 28.9f);
+  REQUIRE(sensorsDataHolder[44].temperatureSensorData == 36.4);
+  REQUIRE(sensorsDataHolder[44].pressureSensorData == 27.3);
+     
+  REQUIRE(sensorsDataHolder[21].temperatureSensorData == 38);
+  REQUIRE(sensorsDataHolder[21].pressureSensorData == 27);
+  
+  REQUIRE(sensorsDataHolder[30].temperatureSensorData == 37.6);
+  REQUIRE(sensorsDataHolder[30].pressureSensorData == 28.9);
 }
